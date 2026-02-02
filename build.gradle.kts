0f199b7e56898e257c1cf3439cd52d1c20653747
nbreval <nicolasbrevalrodriguez@gmail.com>
2026-02-02 07:53:27 +0100

    fix: publish also core module due to dependency errors

--- build.gradle.kts
+++ build.gradle.kts
@@ -54,4 +54,28 @@ subprojects {
             endWithNewline()
         }
     }
+
+    publishing {
+        repositories {
+            maven {
+                name = "GitHubPackages"
+                url = uri("https://maven.pkg.github.com/NicolasBreval/spring-cloud-gateway-scripting")
+                credentials {
+                    username = System.getenv("GITHUB_ACTOR")
+                    password = System.getenv("GITHUB_TOKEN")
+                }
+            }
+        }
+        publications {
+            create<MavenPublication>("mavenJava") {
+                from(components["java"])
+                artifactId = name
+                
+                pom {
+                    description.set("Spring Cloud Gateway filter to modify requests using scripts.")
+                    url.set("https://github.com/NicolasBreval/spring-cloud-gateway-scripting")
+                }
+            }
+        }
+    }
 }
--- groovy/build.gradle.kts
+++ groovy/build.gradle.kts
@@ -1,28 +1,4 @@
 dependencies {
-    implementation(project(":gateway-scripting-core"))
+    api(project(":gateway-scripting-core"))
     implementation(rootProject.libs.apache.groovy)
-}
-
-publishing {
-    repositories {
-        maven {
-            name = "GitHubPackages"
-            url = uri("https://maven.pkg.github.com/NicolasBreval/spring-cloud-gateway-scripting")
-            credentials {
-                username = System.getenv("GITHUB_ACTOR")
-                password = System.getenv("GITHUB_TOKEN")
-            }
-        }
-    }
-    publications {
-        create<MavenPublication>("mavenJava") {
-            from(components["java"])
-            artifactId = "gateway-scripting-groovy"
-            
-            pom {
-                description.set("Spring Cloud Gateway filter to modify requests using scripts.")
-                url.set("https://github.com/NicolasBreval/spring-cloud-gateway-scripting")
-            }
-        }
-    }
 }
